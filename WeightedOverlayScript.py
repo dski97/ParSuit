5040bf0ae3a271724a20cd6c61101bdf3eff20ee
Dominic <46698040+dski97@users.noreply.github.com>
2024-03-12 11:52:47 -0400

    add landuse

--- WeightedOverlayScript.py
+++ WeightedOverlayScript.py
@@ -18,6 +18,7 @@ schools = "Reclass_Schools.tif"
 sewercon = "Reclass_SewerCon.tif"
 slope = "Reclass_Slope.tif"
 wetlands = "Reclass_Wetlands.tif"
+landuse = "landuse.tif"
 
 # RemapValue objects for each raster, based on your specifications
 remap_brownfield = RemapValue([(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), ("NODATA", "NODATA")])
@@ -30,19 +31,39 @@ remap_schools = RemapValue([(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 
 remap_sewerCon = RemapValue([(1, 1), (2, 1), (3, 1), (4, 1), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), ("NODATA", "NODATA")])
 remap_slope = RemapValue([(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), ("NODATA", "NODATA")])
 remap_wetlands = RemapValue([(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), ("NODATA", "NODATA")])
+remap_landuse = RemapValue([
+    ("Impervious Surfaces", 9),
+    ("Developed Open Space", 9),
+    ("Cultivated Land", 8),
+    ("Pasture/Hay", 8),
+    ("Grassland", 8),
+    ("Mixed Forest", 7),
+    ("Scrub/Shrub", 7),
+    ("Palustrine Forested Wetland", 3),
+    ("Palustrine Scrub/Shrub Wetland", 3),
+    ("Palustrine Emergent Wetland", 3),
+    ("Estuarine Emergent Wetland", 3),
+    ("Unconsolidated Shore", 3),
+    ("Bare Land", 10),
+    ("Open Water", "Restricted"), 
+    ("Palustrine Aquatic Bed", "Restricted"), 
+    ("", "NODATA"), 
+    ("NODATA", "NODATA")
+])
 
 # Create a Weighted Overlay table for the TIFF raster files
 myWOTable = WOTable([
-    ["Reclass_Brownfield.tif", 10, "VALUE", remap_brownfield],
-    ["Reclass_BuildableSoil.tif", 10, "VALUE", remap_buildableSoil],
-    ["Reclass_Floodzones.tif", 10, "VALUE", remap_floodzones],
-    ["Reclass_Hospitals.tif", 10, "VALUE", remap_hospitals],
-    ["Reclass_PoliceCommunity.tif", 10, "VALUE", remap_policeCommunity],
-    ["Reclass_Roads.tif", 10, "VALUE", remap_roads],
-    ["Reclass_Schools.tif", 10, "VALUE", remap_schools],
-    ["Reclass_SewerCon.tif", 10, "VALUE", remap_sewerCon],
-    ["Reclass_Slope.tif", 10, "VALUE", remap_slope],
-    ["Reclass_Wetlands.tif", 10, "VALUE", remap_wetlands]
+    ["Reclass_Brownfield.tif", 9, "VALUE", remap_brownfield],
+    ["Reclass_BuildableSoil.tif", 9, "VALUE", remap_buildableSoil],
+    ["Reclass_Floodzones.tif", 9, "VALUE", remap_floodzones],
+    ["Reclass_Hospitals.tif", 9, "VALUE", remap_hospitals],
+    ["Reclass_PoliceCommunity.tif", 9, "VALUE", remap_policeCommunity],
+    ["Reclass_Roads.tif", 9, "VALUE", remap_roads],
+    ["Reclass_Schools.tif", 9, "VALUE", remap_schools],
+    ["Reclass_SewerCon.tif", 9, "VALUE", remap_sewerCon],
+    ["Reclass_Slope.tif", 9, "VALUE", remap_slope],
+    ["Reclass_Wetlands.tif", 9, "VALUE", remap_wetlands],
+    ["landuse.tif", 10 , "VALUE", remap_landuse]
 ], [1, 10, 1])
 
 
