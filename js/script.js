eb3b6dfc7bb72fac083d7769493c145fa4d7b2af
DOMINIC CWAL <dcwalinski97@gmai.com>
2024-03-23 12:49:36 -0400

    add legends

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// script.js
 var map = L.map('map').setView([41.7647, -72.6828], 10);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,33 +5,82 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
 }).addTo(map);
 
+// Function to fetch and add a GeoJSON layer with a specific icon
+function addLayerFromGeoJSON(dataPath, iconName) {
+  fetch(dataPath)
+    .then(response => response.json())
+    .then(data => {
+      L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+          return L.marker(latlng, {
+            icon: L.icon({
+              iconUrl: `icons/${iconName}.png`,
+              iconSize: [18, 18],
+              iconAnchor: [8, 16],
+              popupAnchor: [0, -16]
+            })
+          });
+        }
+      }).addTo(map);
+    });
+}
+
+// Simplified getColor function
+function getColor(gridcode) {
+  if (gridcode >= 0 && gridcode <= 10) {
+    return `hsl(${(gridcode / 10) * 120}, 100%, 50%)`;
+  }
+  return 'gray';
+}
+
+// Add Raster Overlay layer
 fetch('data/RasterOverlay.geojson')
   .then(response => response.json())
   .then(data => {
     L.geoJSON(data, {
       style: function(feature) {
-        var gridcode = feature.properties.gridcode;
-        var color = getColor(gridcode);
         return {
-          fillColor: color,
+          fillColor: getColor(feature.properties.gridcode),
           fillOpacity: 0.7,
-          color: color,
+          color: getColor(feature.properties.gridcode),
           weight: 1
         };
       }
     }).addTo(map);
   });
 
-// Add the ArcGIS feature service for Schools
-L.esri.featureLayer({
-  url: 'https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/Schools_CRCOG/FeatureServer/0',
-  }
-).addTo(map);
+// Adding layers for various features
+addLayerFromGeoJSON('data/Brownfields.geojson', 'brownfield');
+addLayerFromGeoJSON('data/Hospitals.geojson', 'hospital');
+addLayerFromGeoJSON('data/Police Stations.geojson', 'police');
+addLayerFromGeoJSON('data/Schools.geojson', 'school');
 
-function getColor(gridcode) {
-  if (gridcode >= 0 && gridcode <= 10) {
-    var hue = (gridcode / 10) * 120; // Convert gridcode to hue (0-120)
-    return `hsl(${hue}, 100%, 50%)`;
-  }
-  return 'gray'; // Default color for gridcode outside the range
-}
+var legend = L.control.legend({
+    position: 'bottomleft',
+    legends: [{
+      label: "Brownfields",
+      type: "image",
+      url: "icons/brownfield.png",
+      layers: [/* layer reference */]
+    }, {
+      label: "Hospitals",
+      type: "image",
+      url: "icons/hospital.png",
+      layers: [/* layer reference */]
+    }, {
+      label: "Police Stations",
+      type: "image",
+      url: "icons/police.png",
+      layers: [/* layer reference */]
+    }, {
+      label: "Schools",
+      type: "image",
+      url: "icons/school.png",
+      layers: [/* layer reference */]
+    }
+    ]
+  }).addTo(map);
+
+  
+  
+  
