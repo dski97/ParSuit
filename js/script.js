36c13d7d6d51ca89c692a9bfa051177ae8a48f9d
DOMINIC CWAL <dcwalinski97@gmai.com>
2024-03-23 21:38:17 -0400

    add parcels

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var map = L.map('map').setView([41.7647, -72.6828], 10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
   maxZoom: 18,
+  minZoom: 10
 }).addTo(map);
 
 // Function to fetch and add a GeoJSON layer with a specific icon
@@ -55,32 +56,52 @@ addLayerFromGeoJSON('data/Hospitals.geojson', 'hospital');
 addLayerFromGeoJSON('data/Police Stations.geojson', 'police');
 addLayerFromGeoJSON('data/Schools.geojson', 'school');
 
-var legend = L.control.legend({
-    position: 'bottomleft',
-    legends: [{
-      label: "Brownfields",
-      type: "image",
-      url: "icons/brownfield.png",
-      layers: [/* layer reference */]
-    }, {
-      label: "Hospitals",
-      type: "image",
-      url: "icons/hospital.png",
-      layers: [/* layer reference */]
-    }, {
-      label: "Police Stations",
-      type: "image",
-      url: "icons/police.png",
-      layers: [/* layer reference */]
-    }, {
-      label: "Schools",
-      type: "image",
-      url: "icons/school.png",
-      layers: [/* layer reference */]
-    }
-    ]
-  }).addTo(map);
+// Add the feature service layer with minZoom option
+var featureLayer = L.esri.featureLayer({
+  url: 'https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/ParcelsCRCOG/FeatureServer/0'
+}).addTo(map);
 
+var legend = L.control.legend({
+  position: 'bottomleft',
+  legends: [{
+    label: "Brownfields",
+    type: "image",
+    url: "icons/brownfield.png",
+    layers: [/* layer reference */]
+  }, {
+    label: "Hospitals",
+    type: "image",
+    url: "icons/hospital.png",
+    layers: [/* layer reference */]
+  }, {
+    label: "Police Stations",
+    type: "image",
+    url: "icons/police.png",
+    layers: [/* layer reference */]
+  }, {
+    label: "Schools",
+    type: "image",
+    url: "icons/school.png",
+    layers: [/* layer reference */]
+  }]
+}).addTo(map);
+
+// Function to check and update the feature layer based on the current zoom level
+function updateFeatureLayerVisibility() {
+    var currentZoom = map.getZoom();
+    if (currentZoom >= 16) {
+      if (!map.hasLayer(featureLayer)) {
+        map.addLayer(featureLayer); // Add the feature layer if the zoom level is 13 or higher and it's not already added
+      }
+    } else {
+      if (map.hasLayer(featureLayer)) {
+        map.removeLayer(featureLayer); // Remove the feature layer if the zoom level is less than 13
+      }
+    }
+  }
   
+  // Listen for zoom end events to show/hide the feature service layer based on zoom level
+  map.on('zoomend', updateFeatureLayerVisibility);
   
-  
+  // Initial check to set the correct visibility of the feature service layer
+  updateFeatureLayerVisibility();
